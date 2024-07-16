b4a576ec7df4fca0def610069031450a3b469371
Ella Heron <heronsound95@gmail.com>
2024-07-16 13:14:17 -0400

    fix work display maybe

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -5,13 +5,13 @@ import Squiggles from "../components/Squiggles"
 import Grid from "../components/Grid"
 
 export default function Work() {
-  const { title, accent, cards, underGridContent } = attributes
+  const { title, accent, projects, underGridContent } = attributes
   return (
     <>
       <main>
         <Header title={title} accent={accent} />
         <WorkContent />
-        <Grid cards={cards} />
+        <Grid cards={projects} />
         <div>{underGridContent}</div>
       </main>
       <Squiggles page="work" />
