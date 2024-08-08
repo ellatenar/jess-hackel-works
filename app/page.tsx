06162a6557c22d875c852ba7ea4029be91e6dd01
Ella Heron <heronsound95@gmail.com>
2024-08-08 11:50:43 -0400

    delete unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import * as React from "react"
 import { attributes, react as HomeContent } from "content/home.md"
-import styles from "./page.module.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Image from "next/image"
-import headshot from "public/media/headshot.png"
 import Squiggles from "./components/Squiggles"
 
 export default function Home() {
